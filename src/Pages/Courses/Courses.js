1dd933fa00a88d61ee3071e8bfde2f8c77e57f77
atique-atq <atique.atq@gmail.com>
2022-10-26 10:58:06 +0600

    left side done

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -1,15 +1,29 @@
+import Button from 'react-bootstrap/Button';
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
+import LeftSideNav from '../Shared/LeftSideNav/LeftSideNav';
 import './Courses.css';
 
 const Courses = () => {
     const courses = useLoaderData();
 
     return (
-        <div>
-            <h1>This is courses page {courses.length}</h1>
-
-        </div>
+        <div >
+            < Row >
+                <Col md="3" className='left-side-container text-center p-md-2 p-sm-5 ms-0'>
+                    {
+                        courses.map(category => <Button
+                            key={category.id}
+                            variant="light"
+                            className='category-button d-block'>
+                            {category.title}</Button>
+                        )
+                    }
+                </Col>
+                <Col md="9">2 of 2</Col>
+            </Row >
+        </div >
     );
 };
 
