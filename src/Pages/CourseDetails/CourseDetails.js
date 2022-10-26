095e0f1f5b0576c9963da247e0e21b7509df36fa
atique-atq <atique.atq@gmail.com>
2022-10-26 17:36:19 +0600

    added course page

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Card } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom';
 import './CourseDetails.css';
+import Button from 'react-bootstrap/Button';
+import { FaCloudDownloadAlt, FaStar } from "react-icons/fa";
 
 const CourseDetails = () => {
     let courseDetails = useLoaderData();
+    let { title, details, img_url, course_id, author, author_img_url, ratings } = courseDetails;
 
     return (
-        <div className='courseDetails'>
-            <h1>This is our courseDetails page {courseDetails.title}</h1>
-
-        </div>
+        <div>
+            <Card className='card-area'>
+                <div className='d-flex flex-md-row flex-column justify-content-around mt-2'>
+                    <h3 className='text-success fw-bolder me-5'>{title}</h3>
+                    <button className='button button-light border border-0 rounded'> Download <FaCloudDownloadAlt></FaCloudDownloadAlt> </button>
+                </div>
+                <Card.Img variant="top" src={img_url} className="w-75 card-img" />
+                <Card.Body>
+                    <h4>Details of {title} Course:</h4>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <div className='d-flex justify-content-around align-items-center'>
+                        <p>Rating: <span className='text-warning'> {ratings} <FaStar></FaStar> </span> </p>
+                        <p>Author Name: <strong> {author}</strong></p>
+                        <img src={author_img_url} class="w-25 rounded-circle" alt="author" />
+                    </div>
+                    <Link>
+                        <Button variant="primary">Get Premium Access</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+        </div >
     );
 };
 
