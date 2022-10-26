bd7dc7bb292222f0db93ccbe6695fd7c97ca478c
atique-atq <atique.atq@gmail.com>
2022-10-26 13:05:04 +0600

    added courses

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -5,14 +5,14 @@ const Course = ({ courseDetails }) => {
     const { title, details, img_url } = courseDetails;
     return (
         <div className='col-lg-6  mx-lg-0 px-lg-4 my-3 mx-md-3 col-md-11 px-md-3 mx-sm-0'>
-            <div className="card" style={{ height: '32em' }} >
+            <div className="card" style={{ height: '33em' }} >
                 <div className="card-body">
                     <h4 className="card-title">{title}</h4>
                     <div style={{ height: '12em' }} className="text-center p-0 m-0">
-                        <img src={img_url} className="h-75 h-md-100 w-lg-100 w-sm-75" alt="course relevant"></img>
+                        <img src={img_url} className="h-75 h-md-100 w-75 w-md-100" alt="course relevant"></img>
                     </div>
-                    <p className="card-text card-details pt-sm-1 pt-lg-3">{details.slice(0, 175) + '...'}</p>
-                    <a href="/" className="btn btn-primary"> View Details</a >
+                    <p className="card-text card-details pt-sm-1 pt-lg-3 fs-6 fs-md-3">{details.slice(0, 175) + '...'}</p>
+                    <a href="/" className="btn btn-primary m-1"> View Details</a >
                 </div >
             </div >
 
