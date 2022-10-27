d533fe896840f08474a77baba527fea106d4980f
atique-atq <atique.atq@gmail.com>
2022-10-27 20:11:36 +0600

    added pdf converter

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -4,17 +4,26 @@ import { Link, useLoaderData } from 'react-router-dom';
 import './CourseDetails.css';
 import Button from 'react-bootstrap/Button';
 import { FaCloudDownloadAlt, FaStar } from "react-icons/fa";
+import Pdf from "react-to-pdf";
 
 const CourseDetails = () => {
     let courseDetails = useLoaderData();
     let { title, details, img_url, course_id, author, author_img_url, ratings } = courseDetails;
+    const ref = React.createRef();
 
     return (
         <div>
-            <Card className='card-area'>
+            <Card className='card-area' ref={ref}>
                 <div className='d-flex flex-md-row flex-column justify-content-around mt-2'>
                     <h3 className='text-success fw-bolder me-5'>{title}</h3>
-                    <button className='button button-light border border-0 rounded'> Download <FaCloudDownloadAlt></FaCloudDownloadAlt> </button>
+                    <Link>
+
+                        <Pdf targetRef={ref} filename="courseDetails.pdf">
+                            {({ toPdf }) => <Button onClick={toPdf} className='rounded-0' variant="outline-primary">
+                                <FaCloudDownloadAlt></FaCloudDownloadAlt>  DownLoad PDF
+                            </Button>}
+                        </Pdf>
+                    </Link>
                 </div>
                 <Card.Img variant="top" src={img_url} className="w-75 card-img" />
                 <Card.Body>
@@ -25,7 +34,7 @@ const CourseDetails = () => {
                     <div className='d-flex justify-content-around align-items-center'>
                         <p>Rating: <span className='text-warning'> {ratings} <FaStar></FaStar> </span> </p>
                         <p>Author : <strong> {author}</strong></p>
-                        <img src={author_img_url} class="w-25 rounded-circle" alt="author" />
+                        <img src={author_img_url} className="w-25 rounded-circle" alt="author" />
                     </div>
                     <Link to={`/checkout/${course_id}`}>
                         <Button variant="primary">Get Premium Access</Button>
